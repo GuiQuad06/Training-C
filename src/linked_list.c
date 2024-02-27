4a625d79e781baa2d3092ff60e721bbfee7796d5
Guillaume CUADRADO <gucd@oticonmedical.com>
2024-02-27 23:23:51 +0100

    Create basic functions (add, clear , display)

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,16 +1,81 @@
 #include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-linked_list_t *add(int n)
+linked_list_t *head = NULL;
+
+linked_list_t *add_node(int n)
+{
+    linked_list_t *node = NULL;
+
+    node = malloc(sizeof(linked_list_t));
+
+    // Return if the previous malloc failed
+    if (node == NULL)
+        return NULL;
+    
+    // List is empty
+    if (head == NULL)
+    {
+        node->data = n;
+
+        // In our case linked list is filled by the head
+        // which means very first element will be the tail
+        // So its next element will points to NULL
+        node->next = NULL;
+        head = node;
+    }
+    else // List already has nodes
+    {
+        node->data = n;
+
+        // Link the previous HEAD list
+        // and created node becomes the HEAD
+        // Add to the head is O(1)
+        node->next = head;
+        head = node;
+    }
+    return node;
+}
+
+void clear_list(linked_list_t *list)
+{
+    linked_list_t *current = list;
+
+    while(current != NULL)
+    {
+        // Make head list points to next element 
+        if (current == list)
+        {
+            list = current->next;
+        }
+        else // current is no more pointed by list
+        {
+            free(current);
+            // current will point to the next element
+            // so at the tail, it will be current = NULL
+            current = list;
+        }
+    }
+    return;
+}
+
+void print_list(linked_list_t *list)
+{
+    while(list != NULL)
+    {
+        printf("data:%d\n", list->data);
+        list = list->next;
+    }
+    return;
+}
+
+linked_list_t *remove_node(int n)
 {
 
 }
 
-linked_list_t *remove(int n)
-{
-
-}
-
-linked_list_t *insert(int n, int pos)
+linked_list_t *insert_node(int n, int pos)
 {
 
 }
--- src/linked_list.h
+++ src/linked_list.h
@@ -12,8 +12,32 @@ typedef struct {
 } linked_list_t;
 
 // Exported function prototypes
-linked_list_t *add(int n);
-linked_list_t *remove(int n);
-linked_list_t *insert(int n, int pos);
+
+/**
+ * @brief Add a node in a dynamic linked list
+ * The node will be added to the head
+ * @param[in] n : the integer data to add at the head list
+ * @return linked_list_t* a pointer to the outcome linked list
+ */
+linked_list_t *add_node(int n);
+
+/**
+ * @brief Clear all nodes of a dynamic linked list
+ * by performing free() on the elements
+ * @param[in] list : a pointer to the head of linked list
+ * @return none.
+ */
+void clear_list(linked_list_t *list);
+
+/**
+ * @brief Display all node's data of a dynamic linked list
+ * @param[in] list : a pointer to the head of linked list
+ * @return none.
+ */
+void print_list(linked_list_t *list);
+
+
+linked_list_t *remove_node(int n);
+linked_list_t *insert_node(int n, int pos);
 
 #endif // LINKED_LIST_H_
